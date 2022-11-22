c6f64c07f328bf619ac00298b3b02f7653885bef
aseem <aseem.tim@gmail.com>
2022-11-22 13:03:00 -0500

    Created form in react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-function App() {
+import { useState } from "react";
+
+const App = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log(`logged in with`, username, password);
+  };
   return (
     <div>
-      <h1>Welcome</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username:
+          <input
+            type="text"
+            value={username}
+            name="username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password:
+          <input
+            type="password"
+            value={password}
+            name="password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+const users = [
+  {
+    id: 1,
+    email: "test@test.com",
+    user_name: "user_test",
+    mobile_number: "1234567890",
+  },
+  {
+    id: 2,
+    email: "test2@test2.com",
+    user_name: "user_test2",
+    mobile_number: "2234567890",
+  },
+];
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App users={users} />);
