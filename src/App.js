b86dcec2a5ad8bec1a42cd70d1deabf6d5de2f43
aseem <aseem.tim@gmail.com>
2022-12-01 12:47:16 -0500

    Added conditional rendering to login and signup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const loggedUserJson = window.localStorage.getItem("loggedInUser");
+    if (loggedUserJson) {
+      const userInfo = JSON.parse(loggedUserJson);
+      setUser(userInfo.user.user_name);
+    }
+  }, []);
+  console.log(user);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route
+          path="/login"
+          element={!user ? <LoginPage /> : <Navigate replace to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!user ? <SignUpPage /> : <Navigate replace to={"/"} />}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
