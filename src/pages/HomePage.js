7f1236cfbe5514a240488b680ea37ada81fa3cee
aseem <aseem.tim@gmail.com>
2022-12-05 23:03:06 -0500

    Implemented table with time schedules

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,38 +1,87 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import instructorService from "../services/instructor";
+import arrayService from "../services/transposeArray";
+import Notification from "../components/Notification";
 
 const Instructors = () => {
+  const [error, setError] = useState(null);
   const [instructors, setInstructors] = useState([]);
   const [selectValue, setSelectValue] = useState("");
-  //const [setAvailability, setAvailability] = useState("")
-
+  const [availability, setAvailability] = useState(null);
+  const [availableTime, setAvailableTime] = useState([]);
   useEffect(() => {
     (async () => {
       const returnedInstructor = await instructorService.getInstructor();
       setInstructors(returnedInstructor);
-      setSelectValue(instructors[0]._id);
     })();
   }, []);
 
-  const handleDropdown = (event) => {
-    setSelectValue(event.target.value);
+  const handleDropdown = async (event) => {
+    try {
+      setSelectValue(event.target.value);
+      const schedule = await instructorService.getAvailability({
+        id: event.target.value,
+      });
+      setAvailability(schedule);
+      if (schedule) {
+        const result = schedule.map((days) =>
+          availableTime.concat(days.timeslot)
+        );
+        setAvailableTime(arrayService.transpose(result));
+      }
+    } catch (error) {
+      setError("No availablity Found");
+      setTimeout(() => {
+        setError(null);
+      }, 3000);
+    }
   };
 
   return (
     <>
-      <label htmlFor="instructorslist">Choose a instructor:</label>
-      <select
-        name="instructorlist"
-        id="instructorslist"
-        value={selectValue}
-        onChange={handleDropdown}
-      >
-        {instructors.map((elem) => (
-          <option value={elem._id}>{elem.full_name}</option>
-        ))}
-      </select>
+      <div>
+        <Link to="/booking">
+          <button type="button">Book an appointment</button>
+        </Link>
+
+        <select
+          name="instructorlist"
+          id="instructorslist"
+          value={selectValue}
+          onChange={handleDropdown}
+        >
+          <option value="#">Choose an instructor</option>
+          {instructors.map((elem) => (
+            <option value={elem._id}>{elem.full_name}</option>
+          ))}
+        </select>
+
+        <Link to="/appointment">
+          <button type="button">View my appointment</button>
+        </Link>
+      </div>
+
+      <Notification message={error} />
+      {availability && (
+        <table>
+          <tr>
+            {availability.map((header) => (
+              <th>{header.weekday}</th>
+            ))}
+          </tr>
+          {availableTime.map((elements) => (
+            <tr>
+              {elements.map((time) => {
+                const time1 = time.slice(0, 2);
+                const time2 = time.slice(2);
+                return <td>{time1 + ":" + time2}</td>;
+              })}
+            </tr>
+          ))}
+        </table>
+      )}
     </>
   );
 };
--- src/services/instructor.js
+++ src/services/instructor.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import baseUrl from "../url.js";
 const instructorUrl = baseUrl + "/instructor/all";
-const availabilityUrl = baseUrl + "/instructor/:id/availability";
+const availabilityUrl = baseUrl + "/instructor/availability";
 
 const getInstructor = async () => {
   const response = await axios.get(instructorUrl);
   return response.data;
 };
 
-const getAvailability = async (id) => {
-  const response = await axios.get(availabilityUrl);
+const getAvailability = async (body) => {
+  const response = await axios.post(availabilityUrl, body);
   console.log(response.data);
   return response.data;
 };
